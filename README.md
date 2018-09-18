fc8d849bc7f0ce634b5b5044f47dce2d0002ceaf
arshiam2 <arshiam2@illinois.edu>
2018-09-17 22:57:40 -0500

    init

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# script-push
+# script-compiled-pull
 
 `yarn`
 `dotenv yarn start` (make sure you have the mm .env file)
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build . -t gcr.io/mechmania2017/script-push:latest
-docker push gcr.io/mechmania2017/script-push:latest
+docker build . -t gcr.io/mechmania2017/script-compiled-pull:latest
+docker push gcr.io/mechmania2017/script-compiled-pull:latest
 kubectl apply -f app.yaml
 kubectl delete pods -l app=scripts
